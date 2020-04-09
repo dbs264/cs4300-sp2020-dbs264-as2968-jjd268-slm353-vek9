8cee56aadd28c979942bafaa37aa533b34cea1a5
John <jjd268@cornell.edu>
2020-04-09 19:56:53 -0400

    added google json info for nyc bars and nightclubs

--- GooglePlacesApiTest/gatherGoogleInfo.py
+++ GooglePlacesApiTest/gatherGoogleInfo.py
@@ -6,9 +6,9 @@ placeDetailsUrl ="https://maps.googleapis.com/maps/api/place/details/json?"
 types =["bar","night_club"]
 
 def build_place_id_list(file_name):
-	locations = ["40.7233,-74.0030","40.7549,-73.9840","40.7675,-73.9758","40.6959,-73.9956","40.7265,73.9815","40.7150,73.9843","40.7118,74.0131","40.7336,74.0027","40.7396,74.0089","40.7501,74.0028","40.7629,73.9714"]
-	locations += ["40.7813,73.9740","40.8075,73.9626","40.7662,73.9602","40.7081,73.9571","40.6734,74.0083","40.7213,73.9884","40.7460,73.9776","40.7423,73.9801"]
-	radius = "10000"
+	locations = ["40.7233,-74.0030","40.7549,-73.9840","40.7675,-73.9758","40.6959,-73.9956","40.7265,-73.9815","40.7150,-73.9843","40.7118,-74.0131","40.7336,-74.0027","40.7396,-74.0089","40.7501,-74.0028","40.7629,-73.9714"]
+	locations += ["40.7813,-73.9740","40.8075,-73.9626","40.7662,-73.9602","40.7081,-73.9571","40.6734,-74.0083","40.7213,-73.9884","40.7460,-73.9776","40.7423,-73.9801"]
+	radius = "2000"
 
 	place_ids = set()
 
@@ -20,6 +20,8 @@ def build_place_id_list(file_name):
 			# r = requests.get(nearbyUrl+"key="+api_key+"&location="+location+"&radius="+radius+"&type="+t)
 			r = requests.get(nearbyUrl+"key="+api_key+"&location="+location+"&rankby=distance"+"&type="+t)
 			j = r.json()
+			if len(j["results"] )==0:
+				print(j)
 
 			results = j["results"]
 			for place in results:
@@ -56,10 +58,13 @@ def call_place_details(place_id):
 
 def build_place_details_list(place_ids, file_name):
 	places = []
+	count = 0
 	for id in place_ids:
 		place = call_place_details(id)
-		print(place["name"])
 		places.append(place)
+		count += 1
+		if count %100 == 0:
+			print(place["name"])
 
 	
 	with open(file_name, 'w') as outfile:
@@ -70,8 +75,8 @@ def build_place_details_list(place_ids, file_name):
 def main():
 	# build_place_id_list("place_ids.json") #ONLY RUN THIS IF YOU WANT TO WRITE OVER PLACE IDS
 	# build_place_details_list(open_place_id_list("place_ids.json"),"place_details.json")
-	build_place_id_list("by_radius_place_ids.json")
-	# build_place_details_list(open_place_id_list("by_radius_place_ids.json"),"place_details.json")
+	# build_place_id_list("by_radius_place_ids.json")
+	build_place_details_list(open_place_id_list("by_radius_place_ids.json"),"place_details.json")
 
 
 
