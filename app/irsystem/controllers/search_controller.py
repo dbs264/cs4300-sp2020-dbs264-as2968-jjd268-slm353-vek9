200db0b92756e83a1d0711f15ee4e6b11beb668c
Daniel Sanderson <dbs264@cornell.edu>
2020-04-15 13:57:13 -0600

    basic output if no city

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -11,13 +11,21 @@ def search():
 	query = request.args.get('search')
 	city = request.args.get('city')
 	price = request.args.get('price')
-	if not city:
-		city = "new_york"
+	bar_name = request.args.get('bar_name')
+	
 	if not price:
 		price = 5
-	if not query:
-		data = []
-		output_message = ''
+	if not city:
+		if query or bar_name:
+			output_message = 'No results for this city'
+			city = "new_york"
+			data = ["If you did not enter a city, please do so in the form above"]
+		else:
+			output_message = ''
+			data = []
+	elif bar_name:
+		output_message = "Your search: Bars like " + bar_name + " in " + city + " for " + "$" * int(price)
+		data = map(lambda x: x["name"],system.search_data(bar_name,city))
 	else:
 		output_message = "Your search: " + query + " in " + city + " for " + "$" * int(price)
 		data = map(lambda x: x["name"],system.search_data(query,city))
