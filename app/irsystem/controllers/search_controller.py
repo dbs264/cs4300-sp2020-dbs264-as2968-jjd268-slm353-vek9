8253220d061bd57f9aac8cceebf54328e65ed757
Daniel Sanderson <dbs264@cornell.edu>
2020-04-15 20:38:00 -0600

    added comments

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -12,22 +12,31 @@ def search():
 	city = request.args.get('city')
 	price = request.args.get('price')
 	bar_name = request.args.get('bar_name')
+	attribute_list = [request.args.get('attribute1'), request.args.get('attribute2'), request.args.get('attribute3'), request.args.get('attribute4')]
 	
+	#if no price entered assuming max price
 	if not price:
 		price = 5
+	#if no location entered in a search, dont display results
 	if not city:
 		if query or bar_name:
-			output_message = 'No results for this city'
+			output_message = 'Please enter a location above'
 			city = "new_york"
-			data = ["If you did not enter a city, please do so in the form above"]
+			data = [0]
 		else:
 			output_message = ''
 			data = []
+	#if given bar name, retrieve with similar bars
 	elif bar_name:
 		output_message = "Your search: Bars like " + bar_name + " in " + city + " for " + "$" * int(price)
 		data = map(lambda x: x["name"],system.search_data(bar_name,city))
+	#retrieve based on additional preferences, need to factor in presets
 	else:
-		output_message = "Your search: " + query + " in " + city + " for " + "$" * int(price)
+		start = "Your search: " + query + ", " 
+		for i in attribute_list:
+			if i:
+				start += str(i) + ', '
+		output_message = start + " in " + city + " for " + "$" * int(price)
 		data = map(lambda x: x["name"],system.search_data(query,city))
 
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
