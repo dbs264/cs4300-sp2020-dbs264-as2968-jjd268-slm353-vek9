12aa4fe9d744cd0b8ee523ee8a9d9d050e449055
John <jjd268@cornell.edu>
2020-04-19 14:10:03 -0400

    added weighted words depending on length of review

--- app/irsystem/services/initial_search.py
+++ app/irsystem/services/initial_search.py
@@ -1,4 +1,4 @@
-import config, random, json
+import config, random, json, re
 from collections import Counter
 from app.irsystem.services import search_helpers as helpers
 
@@ -29,9 +29,7 @@ def keyword_search(details, word):
 	return helpers.sort_by_score(details)
 
 def multiple_key_word_counts(details, words):
-	print(words)
 	word_list = words.lower().split()
-	print(word_list)
 	for place in details:
 		score = 0
 		if "reviews" in place:
@@ -42,11 +40,30 @@ def multiple_key_word_counts(details, words):
 		place["score"] = score
 	return helpers.sort_by_score(details)
 
+def keyword_counts_normalized(details,words):
+	word_list = words.lower().split()
+	for place in details:
+		score = 0
+		denom = 1
+		if "reviews" in place:
+			for review in place["reviews"]:
+				text_counter = Counter(re.findall(r'\w+',review["text"].lower()))
+				for word in word_list:
+					score += text_counter[word]
+				denom += sum(text_counter.values())
+		place["score"] = score/denom
+	return helpers.sort_by_score(details)
+
+
+
 
 
 def search_data(query, city):
 	details = helpers.load_details(city)
-	top_five = multiple_key_word_counts(details, query)
+	# top_five = multiple_key_word_counts(details, query)
+	# top_five = random_five(details)
+	top_five = keyword_counts_normalized(details, query)
+	# print([[str(x["score"])] + x["reviews"] for x in top_five])
 	return [helpers.format_output(x) for x in top_five]
 
 
