76dae4cdaec40efd67a4088628c24781476c7f75
John <jjd268@cornell.edu>
2020-04-21 17:21:39 -0400

    added bar and preference search endpoints

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -58,3 +58,73 @@ def cities():
 		bars = bars_list.bars_list_for_city(city)
 	return render_template('search.html')
 
+@irsystem.route('/preference_search')
+def preference_search():
+	query = request.args.get('search')
+	city = request.args.get('city')
+	price = request.args.get('price')
+	if not query:
+		query = ''
+	#if no price entered assuming max price
+	if not price:
+		price = 5
+	#if no location entered in a search, dont display results
+	if not city:
+		if query or bar_name:
+			output_message = 'Please enter a location above'
+			city = "new_york"
+			data = [0]
+		else:
+			output_message = ''
+			data = []
+	#if given bar name, retrieve with similar bars
+	elif bar_name:
+		output_message = "Your search: Bars like " + bar_name + " in " + city + " for " + "$" * int(price)
+		data = system.search_data(bar_name,city)
+	#retrieve based on additional preferences, need to factor in presets
+	else:
+		start = "Your search: " + query
+		for i in attribute_list:
+			if i:
+				start += ", " + str(i) 
+		output_message = start + " in " + city + " for " + "$" * int(price)
+		data = system.search_data(query,city)
+
+	return render_template('suggestions.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+
+
+@irsystem.route('/bar_search')
+def bar_search():
+	city = request.args.get('city')
+	price = request.args.get('price')
+	bar_name = request.args.get('bar_name')
+	
+	if not query:
+		query = ''
+	#if no price entered assuming max price
+	if not price:
+		price = 5
+	#if no location entered in a search, dont display results
+	if not city:
+		if query or bar_name:
+			output_message = 'Please enter a location above'
+			city = "new_york"
+			data = [0]
+		else:
+			output_message = ''
+			data = []
+	#if given bar name, retrieve with similar bars
+	elif bar_name:
+		output_message = "Your search: Bars like " + bar_name + " in " + city + " for " + "$" * int(price)
+		data = system.search_data(bar_name,city)
+	#retrieve based on additional preferences, need to factor in presets
+	else:
+		start = "Your search: " + query
+		for i in attribute_list:
+			if i:
+				start += ", " + str(i) 
+		output_message = start + " in " + city + " for " + "$" * int(price)
+		data = system.search_data(query,city)
+
+	return render_template('suggestions.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+
